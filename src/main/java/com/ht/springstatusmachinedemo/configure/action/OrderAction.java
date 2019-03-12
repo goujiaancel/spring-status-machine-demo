e6a4e9e0bfd5bfc4d1d2bbf1b11b52910f119be6
ex-goujia <ex-goujia@htlic.com>
2019-03-12 16:51:04 +0800

    add action and guard

---
+++ src/main/java/com/ht/springstatusmachinedemo/configure/action/OrderAction.java
@@ -0,0 +1,8 @@
+package com.ht.springstatusmachinedemo.configure.action;
+
+/**
+ * @author goujia
+ * @version Id: OrderAction.java, v 0.1 2019/3/12 15:18 goujia Exp $
+ */
+public class OrderAction {
+}
---
+++ src/main/java/com/ht/springstatusmachinedemo/configure/guard/OrderGuard.java
@@ -0,0 +1,8 @@
+package com.ht.springstatusmachinedemo.configure.guard;
+
+/**
+ * @author goujia
+ * @version Id: OrderGuard.java, v 0.1 2019/3/12 15:19 goujia Exp $
+ */
+public class OrderGuard {
+}
